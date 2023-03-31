71e1ea8bff8d87a57f7a9a33a09e22b46439b990
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2023-03-31 14:48:33 +0900

    feat : divide components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2">
+      2023, Designed by Zues
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,58 @@
+const Footer = () => {
+  return (
+    <main>
+      <div className="bg-green-100 max-w-screen-xl mx-auto  h-96 flex justify-center items-center">
+        여기에 나를 소개하는 내용을 적으면 됩니다
+      </div>
+      <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
+        <li className="bg-purple-300 w-60 h-72 rounded-xl">
+          <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+            프로젝트 이미지
+          </div>
+          <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+          <div className="mt-2 mx-2 ">프로젝트 간략한 설명</div>
+        </li>
+        <li className="bg-purple-300 w-60 h-72 rounded-xl">
+          <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+            프로젝트 이미지
+          </div>
+          <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+          <div className="mt-2 mx-2 ">프로젝트 간략한 설명</div>
+        </li>
+        <li className="bg-purple-300 w-60 h-72 rounded-xl">
+          <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+            프로젝트 이미지
+          </div>
+          <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+          <div className="mt-2 mx-2 ">프로젝트 간략한 설명</div>
+        </li>
+        <li className="bg-purple-300 w-60 h-72 rounded-xl">
+          <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+            프로젝트 이미지
+          </div>
+          <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+          <div className="mt-2 mx-2 ">프로젝트 간략한 설명</div>
+        </li>
+        <li className="bg-purple-300 w-60 h-72 rounded-xl">
+          <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+            프로젝트 이미지
+          </div>
+          <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+          <div className="mt-2 mx-2 ">프로젝트 간략한 설명</div>
+        </li>
+        <li className="bg-purple-300 w-60 h-72 rounded-xl">
+          <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+            프로젝트 이미지
+          </div>
+          <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+          <div className="mt-2 mx-2 ">프로젝트 간략한 설명</div>
+        </li>
+      </ul>
+      <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+        내 연락처
+      </div>
+    </main>
+  );
+};
+
+export default Footer;

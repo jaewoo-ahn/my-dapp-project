c82c5766829fa86f1b522ce59c06f02040cd65a4
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2023-03-31 18:48:16 +0900

    card with jsonplaceholder

--- src/Sections/Main/Main.jsx
+++ src/Sections/Main/Main.jsx
@@ -1,17 +1,25 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
 import {PortfolioCard, Introduce, Contact} from "../../Components/index";
 
 const Footer = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/albums/1/photos")
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
+
   return (
     <main>
       <Introduce />
       <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
-        <PortfolioCard />
-        <PortfolioCard />
-        <PortfolioCard />
-        <PortfolioCard />
-        <PortfolioCard />
-        <PortfolioCard />
-        <PortfolioCard />
+        {data.map((res) => {
+          return <PortfolioCard img={res.thumbnailUrl} />;
+        })}
       </ul>
       <Contact />
     </main>
